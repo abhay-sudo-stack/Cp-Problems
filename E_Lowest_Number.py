796fb1e1e1c137ab9e91664ee75ee1ef63f9c184
Abhay-Cherub <abhayasivam@gmail.com>
2026-09-09 21:54:27 +0530

    Problem Solved : 09/09/2026

--- E_Lowest_Number.py
+++ E_Lowest_Number.py
@@ -3,9 +3,8 @@ y= list(map(int,input().split()))
 t=0
 for i in y:
     t=min(y)
-    print(t,end=" ")
     if t==i:
-        print(1-(y.index(i)))
-    break
+        print(t,y.index(i)+1)
+        break
 
     
