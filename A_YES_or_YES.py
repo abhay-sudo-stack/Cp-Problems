c3e36f6708c2b52066c7d3a90b5184ff2a6d2ee5
Abhay-Cherub <abhayasivam@gmail.com>
2026-09-19 14:43:43 +0530

    Problem Solved : 19/09/2026

---
+++ A_YES_or_YES.py
@@ -0,0 +1,15 @@
+x=int(input())
+L=[]
+
+while x>0:
+    y=input()
+    L.append(y)
+    x-=1
+
+for i in L:
+    r=i.lower()
+    if r=="yes":
+        print("YES")
+    else:
+        print("NO")
+   
